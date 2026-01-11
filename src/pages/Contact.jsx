ad9146ef3b764ddfef043d432976a06b823c9f81
Dilakshan Madhusanka <madhusankamrd@gmail.com>
2026-01-11 14:36:54 +0530

    routing update and create pages

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold text-blue-600">Contact Page</h1>
+      <p>This is the Contact Page.</p>
+    </div>
+  )
+}
--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,5 +1,7 @@
 import Home from '@/pages/Home'
 import About from '@/pages/About'
+import Collections from '@/pages/Collections'
+import Contact from '@/pages/Contact'
 import {Routes, Route} from 'react-router-dom'
 import MainLayout from '@/layouts/MainLayout';
 
@@ -9,8 +11,9 @@ export const AppRoutes = () => {
     <Routes>
       <Route element={<MainLayout />}>
         <Route path='/' element={<Home />} />
+        <Route path='/collections' element={<Collections />} />
         <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
       </Route>
     </Routes>
   )
